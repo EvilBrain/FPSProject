044c8cf82a7d3b9006b97c7a6451a16d15d91fa5
GenoWeb <evilbrain.exe@gmail.com>
2022-04-08 23:55:41 +0200

    Ajout d'animations

--- Intermediate/Build/Win64/UE4Editor/Inc/FPSProject/FPSProject.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/FPSProject/FPSProject.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeFPSProject_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x8B5887EF,
-				0xA99A01CC,
+				0xD905F5E1,
+				0x511A47F6,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
--- Source/FPSProject/FPSHUD.h
+++ Source/FPSProject/FPSHUD.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/HUD.h"
+#include "Engine/Canvas.h"
 #include "FPSHUD.generated.h"
 
 /**
@@ -13,5 +14,11 @@ UCLASS()
 class FPSPROJECT_API AFPSHUD : public AHUD
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void DrawHUD() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+		UTexture2D* CrosshairTexture;
 };
